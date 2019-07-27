65cf0f9ac7efc3f0bc5146d0ee35d2c8991c8509
Chi-Hung Chang <chihung861224@gmail.com>
2019-07-27 10:59:00 +0800

    add an event (7/31)

--- js/calendar.js
+++ js/calendar.js
@@ -7,6 +7,7 @@ function get_Events(){
 		[7, 23, '17:30 Volleyball'],
 		[7, 24, '18:30 Baseball Game'],
 		[7, 25, '18:30 Baseball Game'],
+		[7, 31, 'Pingxi'],
 		[8, 11, '9:00 Staff of Basketball Game'],
 		[8, 17, '8:50 Flight to Tokyo'],
 		[8, 21, '19:40 Flight to Taipei']
